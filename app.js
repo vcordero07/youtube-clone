e9278c63924d40d9e7298b70808d37a7aff00af3
vcordero07 <vcordero07@gmail.com>
2017-05-15 23:17:03 -0400

    wip-fully working version

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//05-15-2017
+//missing page token
+
 let endPoint = 'https://www.googleapis.com/youtube/v3/search';
 
 let apiKEY = 'AIzaSyBObrqAWLESv1eKZsNwBG8DBswlsYaIKCU';
@@ -14,25 +17,6 @@ let getDataFromApi = (searchTerm, callback) => {
   $.getJSON(endPoint, query, callback);
 }
 
-//https://www.googleapis.com/youtube/v3/search?
-//part=snippet
-//&maxResults=25
-//&order=relevance
-//&q=site%3Ayoutube.com
-//&topicId=%2Fm%2F02vx4
-//&key={YOUR_API_KEY}
-
-
-//https://www.googleapis.com/youtube/v3/search?
-//pageToken=CBkQAA
-//&part=snippet
-//&maxResults=25
-//&order=relevance
-//&q=site%3Ayoutube.com
-//&topicId=%2Fm%2F02vx4
-//&key={YOUR_API_KEY}
-
-
 let displayData = data => {
   //console.log(data.items[0].id.videoId);
   console.log(data);
@@ -40,6 +24,18 @@ let displayData = data => {
   if (data.items) {
     data.items.forEach(item => {
       resultElement +=
+        //   `
+        // <div class='video-container'>
+        // <div class='video-frame'>
+        // <a class='video-title-link' href="https://www.youtube.com/watch?v=${item.id.videoId}"><img width="246" height="138" src="${item.snippet.thumbnails.medium.url}" frameborder="0" ></img></a>
+        // </div>
+        // <div class='video-info'>
+        // <h3 class='video-title'><a class='video-title-link' href="https://www.youtube.com/watch?v=${item.id.videoId}">${item.snippet.title}</a></h3>
+        // <span class='video-channel'><a href="https://www.youtube.com/user/${item.snippet.channelTitle}">${item.snippet.channelTitle}</a></span>
+        // <span class='video-description'>${item.snippet.description}</span>
+        // </div>
+        // </div>
+        // `;
         `
       <div class='video-container'>
       <div class='video-frame'>
@@ -53,6 +49,7 @@ let displayData = data => {
       </div>
       `;
 
+
     });
   } else {
     resultElement += '<p>No results</p>';
@@ -62,14 +59,16 @@ let displayData = data => {
 }
 
 function watchSubmit() {
-  $('.main-section').hide();
+  $('.video-section').hide();
+  $('.thinktube-footer').hide();
   $('.js-search-form').submit((e) => {
     e.preventDefault();
     let query = $(e.currentTarget).find('.js-query').val();
     if (query === '') {
       console.log('please enter a value');
     } else {
-      $('.main-section').show();
+      $('.video-section').show();
+      $('.thinktube-footer').show();
       getDataFromApi(query, displayData);
     }
 
